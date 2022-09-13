35dc620d200a464f973b1f94587b2f76a77c56f4
Ben C <108667771+BGC1990@users.noreply.github.com>
2022-09-13 16:51:15 +0100

    Added increment and decrement onClick

--- src/Counter.js
+++ src/Counter.js
@@ -3,13 +3,15 @@ import {useState} from 'react';
 export default function Counter(props){
     const [counter, setCounter] = useState(props.startAt);
 
-    setTimeout(() => setCounter(counter + props.countBy), 1000);
+    //setTimeout(() => setCounter(counter + props.countBy), 1000);
 
     return (
         <>
         <p>Start At: {props.startAt}</p>
         <p>Count By: {props.countBy}</p>
         <h4>{counter}</h4>
+        <button className="btn btn-primary" onClick={() => setCounter(counter + props.countBy)}>Increment</button>
+        <button className="bn btn-danger" onClick={() => setCounter(counter - props.countBy)}>Decrement</button>
         </>
     );
 }
