0f8d88680ab7f0d935ff831437eeec0c4529f98c
Ben C <108667771+BGC1990@users.noreply.github.com>
2022-09-21 16:02:04 +0100

    Add files via upload

--- New Project/App.js
+++ New Project/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import {Greet} from './components/Greet'
+import { Message } from './components/Message'
 
 function App() {
   return (
     <div className="App">
-        <Greet />
+        {/* <Greet name='Bruce' heroName='Batman'/>
+        <Greet name='Clark' heroName='Superman'/>
+        <Greet name='Diana' heroName='Wonder Woman'>
+          <button>Action</button>
+          <p>This is children props</p>
+        </Greet> */}
+        <Message />
     </div>
   );
 }
